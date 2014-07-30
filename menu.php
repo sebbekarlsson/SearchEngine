bad0cf4ffedb277e8ffc01a6af9119d17bef30f6
sebbekarlsson <sebbekarlsson97@gmail.com>
2014-07-29 19:58:43 -0700

    Better algorithm

---
+++ menu.php
@@ -0,0 +1,17 @@
+<center>
+<html>
+<link rel="stylesheet" type="text/css" href="style.scss">
+<header>
+	<img src="images/logo.png"/>
+</header>
+
+<body>
+	<div class="faded">
+		
+		<a href="search.php"><img src="images/search.png" width='128px' height='128px'/></a> <a href="shareinfo.php"><img src="images/provide.png" width='128px' height='128px'/></a>
+		
+	</div>
+</body>		
+
+</html>
+</center>
